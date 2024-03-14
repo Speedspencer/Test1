17cbb6b25f81d234523d0a8dfcb94df0103b6faa
Yusaki <puree.marimjie@gmail.com>
2024-03-14 14:29:27 +0700

    X = 12

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    int x = 10;
+    int x = 12;
     // Start is called before the first frame update
     void Start()
     {
